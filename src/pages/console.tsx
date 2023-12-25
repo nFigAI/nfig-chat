4b7707ff77c2498185732df8b79f347a18360bce
Ramees Salim <salim.ramees@gmail.com>
2023-12-25 13:58:14 +0530

    minor change

--- src/pages/console.tsx
+++ src/pages/console.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Chat from "@/components/Chat";
 import { useRouter } from "next/router";
 import { useAuth } from "@/auth/authContext";
